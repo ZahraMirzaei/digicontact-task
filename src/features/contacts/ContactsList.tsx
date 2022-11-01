1c34ef25f7fe00a8a98e68a74c36c6ea5af69614
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-02 00:52:00 +0330

    feat: :sparkles: add context to ContactsList component and add styles

--- src/features/contacts/ContactsList.tsx
+++ src/features/contacts/ContactsList.tsx
@@ -1,5 +1,24 @@
+import { useContact } from "./contactsContext";
+import classes from "./ContactsList.module.scss";
 const ContactsList = () => {
-  return <div>ContactsList</div>;
+  const contactCtx = useContact();
+  let contactList = null;
+
+  if (!contactCtx?.contactsList) {
+    contactList = <div>loading</div>;
+  } else {
+    contactList = contactCtx?.contactsList.map((contact) => (
+      <li className={classes.contactItem} key={contact.phone}>
+        <span>
+          {contact.name.first}, {contact.name.last.toUpperCase()}
+        </span>
+      </li>
+    ));
+  }
+
+  return (
+    <div className={classes.contactsContainer}>{<ul>{contactList}</ul>}</div>
+  );
 };
 
 export default ContactsList;
