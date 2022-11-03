8b98557a2ab4965e6cba1f2f39c9826c0e9ad311
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-03 11:57:26 +0330

    feat: :sparkles: update contactsContext, with activeContactsList, activeLetter, alphabetInfo adn setActiveLetterHandler value

--- src/features/contacts/contactsContext.tsx
+++ src/features/contacts/contactsContext.tsx
@@ -3,31 +3,61 @@ import React, {
   ReactNode,
   useCallback,
   useContext,
+  useRef,
   useState,
 } from "react";
-import { IContact, IContactsContext } from "./contactsInterface";
+import { groupedContactsList } from "../../utils/organizedData";
+import {
+  IContact,
+  IContactsContext,
+  IOrganizedContactList,
+} from "./contactsInterface";
+import { ILabelInfo } from "../label/labelInterface";
 
 const contactsContext = createContext<IContactsContext | null>(null);
 
 function useContact() {
   const contacts = useContext(contactsContext);
-  if (contacts === null) {
+  if (contacts == null) {
     throw new Error("useContact must be used within a ContactsProvider");
   }
   return contacts;
 }
 
 const ContactsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [contactsList, setContactsList] = useState<IContact[] | null>(null);
+  const contactsListRef = useRef<IOrganizedContactList[]>();
+  const labelInfoRef = useRef<ILabelInfo[]>();
+  const [activeLetter, setActiveLetter] = useState("a");
+  const [activeContactsList, setActiveContactsList] =
+    useState<IOrganizedContactList | null>(null);
 
   const setContactsListHandler = useCallback((contactsList: IContact[]) => {
-    if (contactsList) {
-      setContactsList(contactsList);
+    if (contactsList.length) {
+      contactsListRef.current = groupedContactsList(contactsList);
+      labelInfoRef.current = contactsListRef.current.map(
+        (contact: IOrganizedContactList) => ({
+          alphabetLetter: contact.alphabet,
+          recordsLength: contact.record.length,
+        })
+      );
+    }
+  }, []);
+
+  const setActiveLetterHandler = useCallback((letter: string) => {
+    setActiveLetter(letter); //TODO: ask about use state after set in this function.
+    const filteredContactsList = contactsListRef.current!.filter(
+      (contactsList) => contactsList.alphabet === letter
+    );
+    if (filteredContactsList.length > 0) {
+      setActiveContactsList(filteredContactsList[0]);
     }
   }, []);
 
   const value: IContactsContext = {
-    contactsList,
+    activeContactsList,
+    activeLetter,
+    alphabetInfo: labelInfoRef.current,
+    setActiveLetterHandler,
     setContactsListHandler,
   };
 
