dba542b5766e12396b01d53f0304829300b8a2a0
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-03 12:01:17 +0330

    feat: :sparkles: update AlphabetLabel component

--- src/features/label/AlphabetLabel.tsx
+++ src/features/label/AlphabetLabel.tsx
@@ -1,16 +1,32 @@
-import { alphabetArray } from "../../utils/alphabetArray";
+import { useContact } from "../contacts/contactsContext";
 import classes from "./AlphabetLabel.module.scss";
 
 const AlphabetLabel = () => {
+  const { alphabetInfo, activeLetter, setActiveLetterHandler } = useContact();
+
+  const onClickHandler = (letter: string) => {
+    setActiveLetterHandler(letter);
+  };
   return (
-    <div className={classes.container}>
-      {alphabetArray.map((alphabetItem) => (
-        //todo: add number of item beside letter.
-        <span className={classes.alphabetLetter} key={alphabetItem}>
-          {alphabetItem}
-        </span>
-      ))}
-    </div>
+    <ul className={classes.container}>
+      {alphabetInfo
+        ? alphabetInfo.map(({ alphabetLetter, recordsLength }) => {
+            return (
+              <li
+                className={`${classes.alphabetLetter}
+                ${!recordsLength ? classes.disableLetter : ""}
+                ${alphabetLetter === activeLetter ? classes.activeLetter : ""}
+                `}
+                key={alphabetLetter}
+                onClick={() => onClickHandler(alphabetLetter)}
+              >
+                <span>{alphabetLetter}</span>
+                <span className={classes.recordLength}>{recordsLength}</span>
+              </li>
+            );
+          })
+        : null}
+    </ul>
   );
 };
 
