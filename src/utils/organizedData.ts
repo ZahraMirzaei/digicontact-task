f042715642e9ed730cdf5fbe476629166e95ec9b
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-03 11:48:54 +0330

    feat: :sparkles: create groupedContactsList function to groupe contacts list based on alphabet

---
+++ src/utils/organizedData.ts
@@ -0,0 +1,19 @@
+import { IContact } from "../features/contacts/contactsInterface";
+import { alphabetArray } from "./alphabetArray";
+
+export function sortByLastName(contactsList: IContact[]) {
+  return contactsList.sort((a, b) =>
+    a.name.last.localeCompare(b.name.last, "en", { sensitivity: "base" })
+  );
+}
+export function groupedContactsList(contactsList: IContact[]) {
+  sortByLastName(contactsList).forEach((contact: IContact) => {
+    let alphabet = contact.name.last.charAt(0).toLowerCase();
+    alphabetArray.forEach((alphabetItem, index) => {
+      if (alphabetItem.alphabet === alphabet) {
+        alphabetArray[index].record.push(contact);
+      }
+    });
+  });
+  return alphabetArray;
+}
