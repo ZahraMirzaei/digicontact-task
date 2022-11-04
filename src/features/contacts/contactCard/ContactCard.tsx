c45468dbf7e2c2c6f5c3105c440a722cb12620ef
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-04 13:36:32 +0330

    feat: :sparkles: add ContactCard component and imorted in ContactsList component

--- src/features/contacts/contactCard/ContactCard.tsx
+++ src/features/contacts/contactCard/ContactCard.tsx
@@ -1,4 +1,75 @@
+import { IContact } from "../contactsInterface";
 import classes from "./ContactCard.module.scss";
-export const ContactCard = () => {
-  return <div className={classes.cardContainer}>ContactCard</div>;
+import { useShowCard } from "../contactCard/showCardContext";
+
+interface Props {
+  contactInfo: IContact | null;
+}
+export const ContactCard: React.FC<Props> = ({ contactInfo }) => {
+  const { isVisible, setVisibleHandler } = useShowCard();
+
+  function onCloseClickHandler() {
+    setVisibleHandler(false);
+  }
+  return (
+    <>
+      {contactInfo ? (
+        <div
+          className={`${classes.cardContainer}
+          ${isVisible ? classes.showCard : ""}
+        `}
+        >
+          <div className={classes.imgContainer}>
+            <img
+              src={contactInfo.picture.thumbnail}
+              srcSet={`${contactInfo.picture.thumbnail} 500w, 
+                      ${contactInfo.picture.medium} 1000w, 
+                      ${contactInfo.picture.large} 1500w`}
+              alt={contactInfo.login.username}
+            />
+          </div>
+          <div className={classes.useName}>
+            USERNAME &nbsp;{contactInfo.login.username}
+          </div>
+          <div className={classes.infoContainer}>
+            <p className={classes.contactName}>
+              {contactInfo.name.last}, {contactInfo.name.first.toLowerCase()}
+            </p>
+            <div className={classes.contactData}>
+              <div>
+                <span>e-mail</span>
+                <span>{contactInfo.email}</span>
+              </div>
+              <div>
+                <span>phone</span>
+                <span>{contactInfo.phone}</span>
+              </div>
+              <div>
+                <span>street</span>
+                <span>
+                  {contactInfo.location.street.number}{" "}
+                  {contactInfo.location.street.name}
+                </span>
+              </div>
+              <div>
+                <span>city</span>
+                <span>{contactInfo.location.city}</span>
+              </div>
+              <div>
+                <span>state</span>
+                <span>{contactInfo.location.state}</span>
+              </div>
+              <div>
+                <span>postcode</span>
+                <span>{contactInfo.location.postcode}</span>
+              </div>
+            </div>
+          </div>
+          <span className={classes.close} onClick={onCloseClickHandler}>
+            ✖
+          </span>
+        </div>
+      ) : null}
+    </>
+  );
 };
--- src/features/contacts/contactsList/ContactsList.tsx
+++ src/features/contacts/contactsList/ContactsList.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+import { ContactCard } from "../contactCard/ContactCard";
 import { useContact } from "../contactsContext";
+import { useShowCard } from "../contactCard/showCardContext";
 import { IContact } from "../contactsInterface";
 import classes from "./ContactsList.module.scss";
 const ContactsList = () => {
   const { activeContactsList } = useContact();
+  const { isVisible, setVisibleHandler } = useShowCard();
+  const [selectedContact, setSelectedContact] = useState<IContact | null>(null);
   let contactsList = null;
+
+  function onContactClickHandler(contact: IContact) {
+    setSelectedContact(contact);
+    setVisibleHandler(true);
+  }
 
   if (!activeContactsList) {
     contactsList = <div>loading</div>;
   } else {
     contactsList = activeContactsList.record.map((contact: IContact) => (
-      <li className={classes.contactItem} key={contact.id.value}>
+      <li
+        className={`${classes.contactItem} ${
+          selectedContact?.id.value === contact.id.value && isVisible
+            ? classes.selectedContact
+            : ""
+        }`}
+        key={contact.id.value}
+        onClick={() => onContactClickHandler(contact)}
+      >
         <span>
           {contact.name.first}, {contact.name.last.toUpperCase()}
         </span>
@@ -18,7 +36,10 @@ const ContactsList = () => {
   }
 
   return (
-    <div className={classes.contactsContainer}>{<ul>{contactsList}</ul>}</div>
+    <div className={classes.contactsContainer}>
+      <ul>{contactsList}</ul>
+      <ContactCard contactInfo={selectedContact} />
+    </div>
   );
 };
 
