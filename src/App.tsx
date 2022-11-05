64137642f5fbf9b821ad9bfea8156b5dac5a7d2a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-05 14:29:48 +0330

    feat: :sparkles: check loading and error states in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,59 @@
-import { useEffect } from "react";
-import ContactsList from "./features/contacts/contactsList/ContactsList";
-import AlphabetLabel from "./features/label/AlphabetLabel";
+import { useEffect, useState } from "react";
 import { useContact } from "./features/contacts/contactsContext";
 import { getContacts } from "./api";
+import ContactsList from "./features/contacts/contactsList/ContactsList";
+import AlphabetLabel from "./features/label/AlphabetLabel";
 import { ShowCardContextProvider } from "./features/contacts/contactCard/showCardContext";
+import LoadingSpinner from "./features/loadingSpinner/LoadingSpinner";
+import Error from "./features/errorComponent/Error";
 import "./App.scss";
 
 function App() {
-  const { setContactsListHandler, setActiveLetterHandler } = useContact();
+  const { activeContactsList, setContactsListHandler, setActiveLetterHandler } =
+    useContact();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState();
 
   useEffect(() => {
-    getContacts(100).then((response) => {
-      setContactsListHandler(response.data.results);
-      setActiveLetterHandler("a");
-    });
+    setIsLoading(true);
+    getContacts(100)
+      .then((response) => {
+        setContactsListHandler(response.data.results);
+        setActiveLetterHandler("a");
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsError(true);
+        setErrorMessage(e.message);
+        setIsLoading(false);
+        console.log(typeof e.message);
+      });
   }, [setContactsListHandler, setActiveLetterHandler]);
 
-  return (
-    <>
-      <h1 className="digiContact">digiContact</h1>
+  if (isError) {
+    return <Error message={errorMessage} />;
+  }
 
-      <div className="mainContainer">
-        <ShowCardContextProvider>
-          <AlphabetLabel />
-          <ContactsList />
-        </ShowCardContextProvider>
-      </div>
-    </>
-  );
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (activeContactsList?.record.length) {
+    return (
+      <>
+        <h1 className="digiContact">digiContact</h1>
+        <div className="mainContainer">
+          <ShowCardContextProvider>
+            <AlphabetLabel />
+            <ContactsList />
+          </ShowCardContextProvider>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
