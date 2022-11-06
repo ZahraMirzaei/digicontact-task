38d5ed58831911970acb44a34dff43cd8cc8a5c6
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-11-06 19:24:53 +0330

    test: :white_check_mark: add test for App component
    tests for render App and error handler

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { rest } from "msw";
+import { server } from "./mocks/server";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("fetch contacts list from server in App", () => {
+  test("should render digiContact heading", async () => {
+    render(<App />);
+    const digiContactText = await screen.findByRole("heading", {
+      name: "digiContact",
+    });
+    expect(digiContactText).toBeInTheDocument();
+  });
+
+  test("handler error for request fail", async () => {
+    server.resetHandlers(
+      rest.get("https://randomuser.me/api/", (req, res, ctx) =>
+        res(ctx.status(404))
+      )
+    );
+    render(<App />);
+    const errorMessage = await screen.findByText("Sorry, something went wrong");
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
